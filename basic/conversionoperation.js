039df2ff0a408fdb35ee1c72a020c690a5be0838
philips8275 <philipsdsouza12345@gmail.com>
2024-07-23 16:04:40 +0530

    conversion of data type

---
+++ basic/conversionoperation.js
@@ -0,0 +1,13 @@
+// let score = true
+
+// let BooleanNumber = Number (score)
+// console.log(BooleanNumber)
+
+let name = 5
+let bool = Object(name)
+
+console.log(bool);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
+
